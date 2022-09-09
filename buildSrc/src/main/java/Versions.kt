f4d838d8bf0b14340bcd7e37cfb5b710ea6f57de
byansanur <ratbyansanur81@gmail.com>
2022-09-09 14:15:38 +0700

    add config buildSrc libraries

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val gradlePlugin = "7.1.3"
+    const val kotlin = "1.6.21"
+    const val timber = "4.7.1"
+    const val appCompat = "1.5.1"
+    const val coreKtx = "1.9.0"
+    const val material = "1.6.1"
+    const val constraintLayout = "2.1.4"
+    const val jUnit = "4.12"
+}

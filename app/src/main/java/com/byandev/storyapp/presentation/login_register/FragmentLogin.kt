5725a25cb98e1d31d7f0c89dc06eba703a14e1ca
byansanur <ratbyansanur81@gmail.com>
2022-09-13 23:11:16 +0700

    login navigate

--- app/src/main/java/com/byandev/storyapp/presentation/login_register/FragmentLogin.kt
+++ app/src/main/java/com/byandev/storyapp/presentation/login_register/FragmentLogin.kt
@@ -5,18 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.byandev.storyapp.R
+import com.byandev.storyapp.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FragmentLogin : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvRegister.setOnClickListener {
+            findNavController().navigate(FragmentLoginDirections.actionFragmentLoginToFragmentRegister())
+        }
+
+
+
     }
 
 }
